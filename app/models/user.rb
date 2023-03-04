37211408be88dd99d031422be43d46fa89f1b6da
kumapachi <makukuma52@gmail.com>
2023-03-04 23:17:50 +0900

    ユーザー管理機能追加修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :topi
+  with_options presence: true do
+    validates :nickname
+
+    PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+    validates_format_of :password, with: PASSWORD_REGEX, message: 'is invalid. Include both letters and numbers'
+  end
 end
