ab23d0b490fffb49ed5998c123cb25cc87b7c719
kumapachi <makukuma52@gmail.com>
2023-03-04 22:36:58 +0900

    ユーザー登録機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  get 'topics/index'
+  devise_for :users, controllers: { sessions: 'users/sessions'}
   root to: "topics#index"
+  get 'topics/index'
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
 end
