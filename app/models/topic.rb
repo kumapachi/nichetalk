498391dbecfa1f1f690df41e26203942263718a2
kumapachi <makukuma52@gmail.com>
2023-03-12 16:32:25 +0900

    トピック投稿機能の実装

---
+++ app/models/topic.rb
@@ -0,0 +1,12 @@
+class Topic < ApplicationRecord
+
+  belongs_to        :user
+  has_many_attached :images
+
+  validates         :title, presence: true
+  validates         :content, presence: true
+  validates         :images, presence: true
+  validates :images, length: { minimum: 1, maximum: 5, message: "は1枚以上4枚以下にしてください" }
+
+  # validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :topi
+  has_many :topics
   with_options presence: true do
     validates :nickname
 
