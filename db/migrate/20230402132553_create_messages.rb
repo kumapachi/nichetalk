8ded68194f7c938b32ec1f47126f2db1ca2da2ef
kumapachi <makukuma52@gmail.com>
2023-05-17 21:09:19 +0900

    message tableの編集

--- db/migrate/20230402132553_create_messages.rb
+++ db/migrate/20230402132553_create_messages.rb
@@ -3,6 +3,7 @@ class CreateMessages < ActiveRecord::Migration[6.0]
     create_table :messages do |t|
       t.string     :comment, null: false
       t.references :room, null: false, foreign_key: true
+      t.references :topic, null: false, foreign_key: true
       t.references :user, null: false, foreign_key: true
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -36,10 +36,12 @@ ActiveRecord::Schema.define(version: 2023_04_02_140547) do
   create_table "messages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "comment", null: false
     t.bigint "room_id", null: false
+    t.bigint "topic_id", null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["room_id"], name: "index_messages_on_room_id"
+    t.index ["topic_id"], name: "index_messages_on_topic_id"
     t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
@@ -93,6 +95,7 @@ ActiveRecord::Schema.define(version: 2023_04_02_140547) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "messages", "rooms"
+  add_foreign_key "messages", "topics"
   add_foreign_key "messages", "users"
   add_foreign_key "room_users", "rooms"
   add_foreign_key "room_users", "users"
